98a6820c21939182f4a25e59081aaebea5b2464f
Toth Adam <adamtoth988@gmail.com>
2022-09-26 11:32:29 +0200

    2.commit

--- src/lepes.java
+++ src/lepes.java
@@ -2,5 +2,6 @@ public class lepes {
     public static void main(String[] args) {
         System.out.println("Vége");
         System.out.println("Lépés");
+        System.out.println("Heyy");
     }
 }
